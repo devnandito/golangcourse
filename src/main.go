1b1cd0e0839ef86a4f8c17e57eb0908fac81c4cd
devnandito <fhersa@gmail.com>
2021-03-04 16:33:35 -0300

    arrslices

--- src/main.go
+++ src/main.go
@@ -3,24 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// Defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	var array [4]int
+	fmt.Println(array)
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue y break
+	// Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-	for i :=0 ; i < 10; i++ {
-		fmt.Println(i)
-		//Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
-
-		// Break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	// Metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 }
