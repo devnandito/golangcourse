1e75bb7c8ea5ec5d97663708d37a6e9160a85933
devnandito <fhersa@gmail.com>
2021-03-04 09:43:26 -0300

    punteros

--- src/main.go
+++ src/main.go
@@ -1,15 +1,45 @@
 package main
 
-import (
-	pk "curso_golang_platzi/src/mypackage"
-	"fmt"
-)
+import "fmt"
 
-func main () {
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	fmt.Println(myCar)
+type pc struct {
+	ram int
+	disk int
+	brand string
+}
 
-	pk.PrintMessage("Hola platzi")
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "Pong")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
+
+func (myPc pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d DB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
+}
+
+func main() {
+	a := 50
+	b := &a
+
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{ram:16, disk: 200, brand: "msi"}
+	fmt.Println(myPc)
+
+	myPc.ping()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+	
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+	
+	fmt.Println(myPc)
 }
