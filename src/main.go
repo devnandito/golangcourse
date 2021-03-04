d8a1b16cda7ba0164e88b816d80d44730508f092
devnandito <fhersa@gmail.com>
2021-03-04 13:05:05 -0300

    modules

--- src/main.go
+++ src/main.go
@@ -1,39 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func message(text string, c chan string) {
-	c <- text
-}
+	"github.com/labstack/echo"
+)
 
 func main() {
-	c := make(chan string, 2)
-	c <- "Mesaje1"
-	c <- "Mensaje2"
-
-	fmt.Println(len(c), cap(c))
-
-	// Range y close
-	close(c)
-	
-	// c <- "Mensaje3"
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	// Select
-	email1 := make(chan string)
-	email2 :=make(chan string)
-	go message("mensaje1", email1)
-	go message("mensaje2", email2)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <- email1:
-			fmt.Println("Email recibido de email1", m1)
-		case m2 := <- email2:
-			fmt.Println("Email recibido de email2", m2)
-		}
-	}
-
+	// Instanciar echo
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
